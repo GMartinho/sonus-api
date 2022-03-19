d51f6f9ed2cf4db294a541ba81ff84b27b2db03d
Gabriel Martinho <gabriel.martinho@b2wdigital.com>
2022-03-19 03:27:47 -0300

    feat: create create-user.dto.ts and related provisional changes

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  user_account,
+  Prisma,
+} from '@prisma/client';
 
 @Injectable()
 export class UserService {
+
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user_account.create({
+      data: {
+        first_name: createUserDto.firstName,
+        last_name: createUserDto.lastName,
+        email: createUserDto.email,
+        password: createUserDto.password
+      }
+    })
   }
 
   findAll() {
