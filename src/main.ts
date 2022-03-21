8cca9d9270d543d2906a57f0f23756d55691f944
Gabriel Martinho <gabriel.martinho@b2wdigital.com>
2022-03-20 21:27:03 -0300

    feat: install swagger and set others configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Sonus API')
+    .setDescription('Node.js API built with NestJS for the Sonus project. Where your compositions come from the heart to the recording.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, Length } from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
+    @Length(2, 255)
     firstName: string;
+
+    @IsNotEmpty()
+    @Length(2, 255)
     lastName: string;
+
+    @IsNotEmpty()
+    @Length(7, 320)
     email: string;
+
+    @IsNotEmpty()
+    @Length(8, 255)
     password: string;
 }
